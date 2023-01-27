5bb7cc985de77f575c98a07b464f0458c45003e2
krishna ingole <ingolek21999@gmail.com>
2023-01-27 11:53:54 +0530

    files added

---
+++ config.py
@@ -0,0 +1,5 @@
+
+
+MODEL_FILE_PATH = r'C:\Users\ingol\Desktop\Python\git & flask practice\Day 3(21 jan)\project_app\Linear_Model.pkl'
+
+JSON_FILE_PATH = r'C:\Users\ingol\Desktop\Python\git & flask practice\Day 3(21 jan)\project_app\project_data.json'
